85d63fdb3c0fc8e6bb395bd95c80af7567155c2b
kstadnicka <kamila.kudyba93@gmail.com>
2024-07-23 12:36:15 +0200

    Create data test

---
+++ your-books/src/main/resources/db/testdata/0001_category.sql
@@ -0,0 +1,10 @@
+insert into category (name) values ('Romance');
+insert into category (name) values ('Thriller');
+insert into category (name) values ('Drama');
+insert into category (name) values ('History');
+insert into category (name) values ('Autobiography');
+insert into category (name) values ('Crime');
+insert into category (name) values ('Children');
+insert into category (name) values ('Action');
+insert into category (name) values ('Comedy');
+insert into category (name) values ('Horror');
