cc2c3190a4155587534b4cfd3244d00e1cb1819a
kstadnicka <kamila.kudyba93@gmail.com>
2024-07-29 12:18:10 +0200

    404 error page implementations

--- your-books/src/main/java/org/ks/yourbooks/web/BookController.java
+++ your-books/src/main/java/org/ks/yourbooks/web/BookController.java
@@ -2,10 +2,12 @@ package org.ks.yourbooks.web;
 
 import org.ks.yourbooks.domain.book.BookService;
 import org.ks.yourbooks.domain.book.dto.BookDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -19,8 +21,9 @@ public class BookController {
 
     @GetMapping("/book/{id}")
     public String getBook(@PathVariable long id, Model model){
-        Optional<BookDto> optionalBook = bookService.findBookById(id);
-        optionalBook.ifPresent(book-> model.addAttribute("book", book));
+        BookDto book = bookService.findBookById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        model.addAttribute("book",book);
         return "book";
     }
 }
