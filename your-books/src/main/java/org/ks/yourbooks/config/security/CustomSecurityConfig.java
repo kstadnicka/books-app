72a611c1b81823bf1fb926b56d912f7373a01827
kstadnicka <kamila.kudyba93@gmail.com>
2024-08-07 14:56:09 +0200

    create possibility of registration and disabling CSRF for the H2 console

--- your-books/src/main/java/org/ks/yourbooks/config/security/CustomSecurityConfig.java
+++ your-books/src/main/java/org/ks/yourbooks/config/security/CustomSecurityConfig.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class CustomSecurityConfig {
@@ -19,17 +19,18 @@ public class CustomSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authz) -> authz
-                .requestMatchers("/admin/**").hasAnyRole(EDITOR_ROLE, ADMIN_ROLE)
-                .anyRequest().permitAll()
-        )
+                        .requestMatchers("/admin/**").hasAnyRole(EDITOR_ROLE, ADMIN_ROLE)
+                        .anyRequest().permitAll()
+                )
                 .formLogin(login -> login
                         .loginPage("/login").permitAll()
                 )
                 .logout(logout -> logout
                         .logoutRequestMatcher(new AntPathRequestMatcher("/logout/**", HttpMethod.GET.name()))
-                        .logoutSuccessUrl("/login?logout")
+                        .logoutSuccessUrl("/login?logout").permitAll()
                 );
-
+        http.csrf().ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+        http.headers().frameOptions().sameOrigin();
         return http.build();
     }
 
@@ -40,5 +41,10 @@ public class CustomSecurityConfig {
                 "/scripts/**",
                 "/styles/**"
         );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
---
+++ your-books/src/main/java/org/ks/yourbooks/domain/user/dto/UserRegistrationDto.java
@@ -0,0 +1,9 @@
+package org.ks.yourbooks.domain.user.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRegistrationDto {
+    private String email;
+    private String password;
+}
